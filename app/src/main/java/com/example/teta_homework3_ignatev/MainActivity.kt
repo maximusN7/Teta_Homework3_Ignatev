ca2f3e50b232708df1ec98c47898c9a2921e7f9d
mrignatev <maximarius78@gmail.com>
2022-09-23 16:21:06 +0300

    init commit

---
+++ app/src/main/java/com/example/teta_homework3_ignatev/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.teta_homework3_ignatev
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.teta_homework3_ignatev.ui.theme.Teta_Homework3_IgnatevTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Teta_Homework3_IgnatevTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Teta_Homework3_IgnatevTheme {
+        Greeting("Android")
+    }
+}
